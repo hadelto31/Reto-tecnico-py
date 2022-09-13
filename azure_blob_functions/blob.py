45b31a376345697cb094ae4ec54b74f707c156e0
harolete10 <hadelto@hotmail.com>
2022-09-12 21:59:07 -0500

    funcion para subir un blob

--- azure_blob_functions/blob.py
+++ azure_blob_functions/blob.py
@@ -1,4 +1,15 @@
 from azure.storage.blob import BlobServiceClient
 from os import getenv
+from typing import BinaryIO
 
-BlobServiceClient.from_connection_string(getenv("AZURE_STORAGE_CONNECTION_STRING"))
+blob_service_client = BlobServiceClient.from_connection_string(getenv("AZURE_STORAGE_CONNECTION_STRING"))
+
+
+def upload_blob(filename: str, container: str, data: BinaryIO):
+    try:
+        blob_client = blob_service_client.get_blob_client(container=container, blob=filename)
+
+        blob_client.upload_blob(data)
+    except Exception as e:
+        pass
+      
